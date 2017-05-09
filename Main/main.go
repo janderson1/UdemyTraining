1beab10b22247cabce35029d741de86b4e68cb27
janderson <janderson@peoplenetonline.com>
2017-05-09 13:20:48 -0500

    Second project

---
+++ Main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/janderson1/UdemyTraining/stringutil"
+
+
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+
+}
